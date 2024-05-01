c68dfc61eeedd791efd6c0ae61fac79ddac67ae5
Borshadevi <borshadevi27@gmail.com>
2024-05-01 21:28:45 +0600

    add my first commit

---
+++ src/Route/Route.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Root/Root";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root></Root>,
+      children:[
+        {
+            
+        }
+      ]
+    },
+  ]);
+  export default router;
